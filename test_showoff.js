bc0174e5ecd879f18ea530e9e8d7ceea79f9043b
Otto Hockel <hockel.otto@gmail.com>
2015-09-17 23:33:06 +0200

    more testing fun

--- test_showoff.js
+++ test_showoff.js
@@ -3,12 +3,41 @@ var expect = chai.expect;
 describe("Showoff.init()", function() {
 
   describe("slideIndex", function() {
-    it("should be zero", function() {
+    it("defaults to zero", function() {
       var config = {
           updateProgressBar: function() {return true;}
       };
-      var so = Showoff.init(config);
-      expect(so.slideIndex).to.equal(0);
+      Showoff.init(config);
+      expect(Showoff.slideIndex).to.equal(0);
+    });
+  });
+  describe("bound", function() {
+    it("should be an empty array", function() {
+      var config = {
+          updateProgressBar: function() {return true;}
+      };
+      Showoff.init(config);
+      expect(typeof Showoff.bound).to.equal(typeof []);
+      expect(Showoff.bound.length).to.equal([].length);
+    });
+  });
+  describe("slideClass", function() {
+    it("defaults to 'slide'", function() {
+      var config = {
+          updateProgressBar: function() {return true;}
+      };
+      Showoff.init(config);
+      expect(Showoff.slideClass).to.equal("slide");
+    });
+  });
+  describe("#progress", function() {
+    it("should return a value between 0 and 100", function() {
+      var config = {
+          updateProgressBar: function() {return true;}
+      };
+      Showoff.init(config);
+      expect(Showoff.progress()).to.be.above(-1);
+      expect(Showoff.progress()).to.be.below(101);
     });
   });
 });
